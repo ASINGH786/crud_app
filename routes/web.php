b3b4f0c0840de025d193cfa7b9897ca703bcd395
Amar Singh <singhnahar@icloud.com>
2020-11-01 21:42:18 +0000

    Added StudentControler

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::resource('/student', StudentController::class);
+
